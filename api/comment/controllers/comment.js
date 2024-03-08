3418c5ef371f5f84e37e0b79ae08b168b472f9cd
wannabefrontdeveloper <giwonk@naver.com>
2024-03-09 00:35:39 +0900

    오타수정

--- api/comment/controllers/comment.js
+++ api/comment/controllers/comment.js
@@ -4,24 +4,23 @@ module.exports = {
   async create(ctx) {
     // 사용자 id를 데이터에 추가
     ctx.request.body.user = ctx.state.user.id;
-    // article id 설정
-    ctx.request.body.article = ctx.params.articleId;
+    const { articleId } = ctx.params;
+    ctx.request.body.article = articleId;
 
     // 게시글 존재 유무 확인
-    // id로 데이터 조회
-    const article = await strapi.services.article.findOne({ id: articleId });
+    const article = await strapi.services.article.findOne({ id: articleId }); // id 로 데이터 조회
     if (!article) {
       ctx.throw(404);
     }
 
     // Comment 데이터 생성
-    const entity = await createStrapi.services.article.create(ctx.request.body);
+    const entity = await strapi.services.comment.create(ctx.request.body);
     // 응답 반환
     return sanitizeEntity(entity, { model: strapi.models.comment });
   },
   async find(ctx) {
     // articleId로 댓글 조회
-    const entities = await strapi.services.commnet.find({
+    const entities = await strapi.services.comment.find({
       article: ctx.params.articleId,
     });
     // 각 데이터에 대해 sanitizeEntity를 처리하여 응답 반환
@@ -46,7 +45,7 @@ module.exports = {
     }
     // 사용자 확인
     if (ctx.state.user.id !== comment.user.id) {
-      return ctx.unauthroized(`You can't update this entry`);
+      return ctx.unauthorized(`You can't update this entry`);
     }
     // comment  데이터 업데이트
     const entity = await strapi.services.comment.update(
@@ -72,7 +71,7 @@ module.exports = {
 
     // 사용자 확인
     if (ctx.state.user.id !== comment.user.id) {
-      return ctx.unauthroized(`You can't remove this entry`);
+      return ctx.unauthorized(`You can't remove this entry`);
     }
 
     // 데이터 삭제
