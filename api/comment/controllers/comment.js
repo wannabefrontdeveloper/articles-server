3e011de90d3e172c56449855fb16d4e96c44ed70
wannabefrontdeveloper <giwonk@naver.com>
2024-03-09 00:08:06 +0900

    코드추가 댓글을 작성하는 핸들러를 구현하였습니다.

--- api/comment/controllers/comment.js
+++ api/comment/controllers/comment.js
@@ -1,8 +1,14 @@
-'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    // 사용자 id를 데이터에 추가
+    ctx.request.body.user = ctx.state.user.id;
+    // article id 설정
+    ctx.request.body.article = ctx.params.articleId;
+    // Comment 데이터 생성
+    const entity = await createStrapi.services.article.create(ctx.request.body);
+    // 응답 반환
+    return sanitizeEntity(entity, { model: strapi.models.comment });
+  },
+};
