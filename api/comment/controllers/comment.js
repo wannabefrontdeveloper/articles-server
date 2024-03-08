f64681c157c7ea8712a9353e1c4a9768d0fba256
wannabefrontdeveloper <giwonk@naver.com>
2024-03-09 00:10:36 +0900

    코드수정 해당 게시글이 존재하는지 확인하는 작업을 추가로 구현하였습니다.

--- api/comment/controllers/comment.js
+++ api/comment/controllers/comment.js
@@ -6,6 +6,14 @@ module.exports = {
     ctx.request.body.user = ctx.state.user.id;
     // article id 설정
     ctx.request.body.article = ctx.params.articleId;
+
+    // 게시글 존재 유무 확인
+    // id로 데이터 조회
+    const article = await strapi.services.article.findOne({ id: articleId });
+    if (!article) {
+      ctx.throw(404);
+    }
+
     // Comment 데이터 생성
     const entity = await createStrapi.services.article.create(ctx.request.body);
     // 응답 반환
