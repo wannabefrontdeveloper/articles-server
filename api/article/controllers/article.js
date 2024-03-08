54c73c482793e311de2a89b50d1f85a571f64e04
wannabefrontdeveloper <giwonk@naver.com>
2024-03-08 23:21:16 +0900

    코드수정 새 게시글을 작성할 수 있게 해주는 create 핸들러 함수를 작성하였습니다.

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,8 +1,10 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    // 사용자의 id를 데이터에 추가
+    ctx.request.body.user = ctx.state.user.id;
+    // article 데이터 생성
+    const entity = await strapi.services.article.create(ctx.request.body);
+    // 잘못된 필드 및 Private 값 제외하고 반환
+    return sanitizeEntitiy(entity, { model: strapi.models.article });
+  },
+};
